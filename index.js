8fcd62a68e4a2635e0b436439afe076d06c63378
sibStephen <sibu.stephen@srijan.in>
2016-06-12 21:34:33 +0530

    Reverted strucutral changes

--- index.js
+++ index.js
@@ -4,4 +4,52 @@
     .module('uiform', [
       'dashboard'
     ]);
+    function appRun(routerHelper) {
+      var otherwise = '/dashboard';
+      routerHelper.configureStates(getStates(), otherwise);
+
+    }
+
+    angular
+      .module('dashboard')
+      .run(appRun);
+
+
+
+    function getStates() {
+      return [{
+        state: '404',
+        config: {
+          url: '/404',
+          templateUrl: 'app/404.html',
+          title: '404'
+        }
+      }];
+    }
+    /**
+     * Function to configure ui-states for router
+     * @param  {[type]} routerHelper [description]
+     */
+    function _appRun(routerHelper) {
+      routerHelper.configureStates(_getStates());
+    }
+
+    module.run(_appRun);
+
+    /**
+     * Function to configure state for module
+     * @return {Array} Array of ui-states
+     */
+    function _getStates() {
+      return [{
+        state: 'layout.dashboard',
+        config: {
+          url: '/dashboard',
+          templateUrl: 'dashboard/dashboard.html',
+          controller: 'dashboardController as vm',
+          title: 'dashboard',
+          authRequired: true
+        }
+      }];
+    }
 })();
